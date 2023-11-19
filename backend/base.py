f539ac2ea1918580fc537e63251bba5484bb0ee0
iamkartiks <kartikeysinha40@gmail.com>
2023-11-19 14:35:35 +0530

    initial setup

---
+++ backend/base.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+
+if __name__ == '__main__':
+    app.run(port=3000)
+
+
